9f9269fe1178ee04b6e6a5c09cffbfd4fead79dc
pfishe13 <76025363+pfishe13@users.noreply.github.com>
2021-11-29 17:03:09 -0500

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(string inp){
+    for( int i = 0; i < inp.size()/2; ++i ){
+        if ( inp[i] != inp[inp.size()-1-i] ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    string input;
+    cout << "Enter a word to check if it is a palindrome: " << endl;
+    cin >> input;
+    
+    bool flag;
+    flag = isPalindrome(input);
+    
+    if (flag) {
+        cout << input << " is a palindrome" << endl;
+    } else {
+        cout << input << " is not a palindrome" << endl;
+    }
+    
+    return 0;
+}
