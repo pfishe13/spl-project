265a933006ebb04068c7a53b115dbe88749bd1da
pfishe13 <76025363+pfishe13@users.noreply.github.com>
2021-11-30 14:43:22 -0500

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
+#include <iostream>                                                 // mandatory file for input/output
 
-using namespace std;
+using namespace std;                                                // for standard namespace
 
-bool isPalindrome(string inp){
-    for( int i = 0; i < inp.size()/2; ++i ){
-        if ( inp[i] != inp[inp.size()-1-i] ) {
+bool isPalindrome(string inp){                                      // return type has to be explicitly specified, parameter type also has to be specified
+    for( int i = 0; i < inp.size()/2; ++i ){                        // iterate i from 0 to half of the size of the inp string
+        if ( inp[i] != inp[inp.size()-1-i] ) {                      // if characters do not match, return false
             return false;
         }
     }
-    return true;
+    return true;                                                    // all characters match, inp is a palindrome, return true
 }
 
-int main()
+int main()                                                          // required main function that all c++ programs need
 {
-    string userInput;
-    cout << "Enter a word to check if it is a palindrome: ";
-    cin >> userInput;
+    string userInput;                                               // data type 'string' required in c++
+    cout << "Enter a word to check if it is a palindrome: ";        // output string to console
+    cin >> userInput;                                               // get user input from console and store in userInput
     
-    bool flag;
-    flag = isPalindrome(userInput);
+    bool flag;                                                      // data type 'bool' required
+    flag = isPalindrome(userInput);                                 // store result of function return value in flag (true if userInput is a palindrome, false otherwise)
     
-    if (flag) {
+    if (flag) {                                                     // if flag is true
         cout << userInput << " is a palindrome" << endl;
-    } else {
+    } else {                                                        // if flag is false
         cout << userInput << " is not a palindrome" << endl;
     }
     
-    return 0;
+    return 0;                                                       // return 0 to end main function
 }
