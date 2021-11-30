b2622fceb20e6e6fce2adb0e8f7d1cd42b06dcf0
pfishe13 <76025363+pfishe13@users.noreply.github.com>
2021-11-30 14:55:50 -0500

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,14 @@
-def isPalindrome(userInp):                                                  // return type does not need to be specified in python. parameter does not need type either
-    for i in range(0, int(len(userInp)/2)):                                 // iterate i from 0 to half of the size of the inp string
-        if userInp[i] != userInp[len(userInp)-1-i]:                         // if characters do not match, return false
+def isPalindrome(userInp):                                                  # return type does not need to be specified in python. parameter does not need type either
+    for i in range(0, int(len(userInp)/2)):                                 # iterate i from 0 to half of the size of the inp string
+        if userInp[i] != userInp[len(userInp)-1-i]:                         # if characters do not match, return false
             return False
-    return True                                                             // all characters match, inp is a palindrome, return true
+    return True                                                             # all characters match, inp is a palindrome, return true
 
-userInput = input("Enter a word to check if it is a palindrome: ")          // type is not specified when userInput is declared. userInput is declared, the string is outputted to the console and the user input is assigned to userInput all in one line
+userInput = input("Enter a word to check if it is a palindrome: ")          # type is not specified when userInput is declared. userInput is declared, the string is outputted to the console and the user input is assigned to userInput all in one line
 
-flag = isPalindrome(userInput);                                             // type is not specified when flag is declared. flag is set to the return value of isPalindrome
+flag = isPalindrome(userInput);                                             # type is not specified when flag is declared. flag is set to the return value of isPalindrome
 
-if flag:                                                                    // if flag is true
+if flag:                                                                    # if flag is true
     print(userInput + " is a palindrome")
-else:                                                                       // if flag is false
+else:                                                                       # if flag is false
     print(userInput + " is not a palindrome")
